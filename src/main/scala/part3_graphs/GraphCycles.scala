e2382844e4200040a904d5b56095791759a006d2
Alejandro <atorresato@gmail.com>
2022-07-28 11:43:32 +0200

    Fibonacci using Akka Streams

--- src/main/scala/part3_graphs/GraphCycles.scala
+++ src/main/scala/part3_graphs/GraphCycles.scala
@@ -1,8 +1,18 @@
 package part3_graphs
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Flow, GraphDSL, Merge, MergePreferred, RunnableGraph, Source}
-import akka.stream.{ClosedShape, OverflowStrategy, SystemMaterializer}
+import akka.stream.scaladsl.{
+  Broadcast,
+  Flow,
+  GraphDSL,
+  Merge,
+  MergePreferred,
+  RunnableGraph,
+  Sink,
+  Source,
+  Zip
+}
+import akka.stream.{ClosedShape, OverflowStrategy, SystemMaterializer, UniformFanInShape}
 
 object GraphCycles extends App {
 
@@ -11,8 +21,7 @@ object GraphCycles extends App {
 
   val accelerator = GraphDSL.create() { implicit builder =>
     import GraphDSL.Implicits._
-
-    val sourceShape = builder.add(Source(1 to 100))
+    val sourceShape = builder.add(Source(1 to 1000))
     val mergeShape  = builder.add(Merge[Int](2))
     val incrementerShape = builder.add(Flow[Int].map { x =>
       println(s"Accelerating $x")
@@ -23,19 +32,17 @@ object GraphCycles extends App {
     mergeShape <~ incrementerShape
 
     ClosedShape
+
   }
 
-  // RunnableGraph.fromGraph(accelerator).run() // Graph cycle deadlock! -> Backpressure
+  // RunnableGraph.fromGraph(accelerator).run() // graph cycle deadlock
 
-  // Solutions
   /*
   Solution 1: MergePreferred
    */
-
   val actualAccelerator = GraphDSL.create() { implicit builder =>
     import GraphDSL.Implicits._
-
-    val sourceShape = builder.add(Source(1 to 100))
+    val sourceShape = builder.add(Source(1 to 1000))
     val mergeShape  = builder.add(MergePreferred[Int](1))
     val incrementerShape = builder.add(Flow[Int].map { x =>
       println(s"Accelerating $x")
@@ -46,29 +53,79 @@ object GraphCycles extends App {
     mergeShape.preferred <~ incrementerShape
 
     ClosedShape
+
   }
 
   // RunnableGraph.fromGraph(actualAccelerator).run()
 
   /*
-  Solution 2: Buffers
+  Solution 2: buffers
    */
 
   val bufferedAccelerator = GraphDSL.create() { implicit builder =>
     import GraphDSL.Implicits._
-
-    val sourceShape = builder.add(Source(1 to 100))
+    val sourceShape = builder.add(Source(1 to 1000))
     val mergeShape  = builder.add(Merge[Int](2))
-    val incrementerShape = builder.add(Flow[Int].buffer(10, OverflowStrategy.dropHead).map { x =>
+    val repeater = builder.add(Flow[Int].buffer(10, OverflowStrategy.dropHead).map { x =>
       println(s"Accelerating $x")
+      Thread.sleep(100)
       x
     })
 
-    sourceShape ~> mergeShape ~> incrementerShape
-    mergeShape <~ incrementerShape
+    sourceShape ~> mergeShape ~> repeater
+    mergeShape <~ repeater
+
+    ClosedShape
+
+  }
+
+  // RunnableGraph.fromGraph(bufferedAccelerator).run()
+
+  /**
+   * Challenge: create a fan-in shape
+   *   - two inputs which will be fed with EXACTLY ONE number (1 and 1)
+   *   - output will emit an INFINITE FIBONACCI SEQUENCE based of those 2 numbers 1,2,3,5,8 ...
+   *
+   * Hint: Use ZipWith and cycles, MergePreferred
+   */
+
+  val fibonacciGraph = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val inputZip = builder.add(Zip[BigInt, BigInt])
+    val merge    = builder.add(MergePreferred[(BigInt, BigInt)](1))
+    val logic = builder.add(Flow[(BigInt, BigInt)].map { pair =>
+      val last     = pair._1
+      val previous = pair._2
+      Thread.sleep(100)
+      (last + previous, last)
+    })
+
+    val broadcast   = builder.add(Broadcast[(BigInt, BigInt)](2))
+    val extractLast = builder.add(Flow[(BigInt, BigInt)].map(_._1))
+
+    inputZip.out ~> merge ~> logic ~> broadcast ~> extractLast
+    merge.preferred <~ broadcast
+
+    UniformFanInShape(extractLast.out, inputZip.in0, inputZip.in1)
+
+  }
+
+  val graph = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val source1 = builder.add(Source.single[BigInt](1))
+    val source2 = builder.add(Source.single[BigInt](1))
+    val sink    = builder.add(Sink.foreach(println))
+    val fibo    = builder.add(fibonacciGraph)
+
+    source1 ~> fibo.in(0)
+    source2 ~> fibo.in(1)
+
+    fibo.out ~> sink
 
     ClosedShape
   }
 
-  RunnableGraph.fromGraph(bufferedAccelerator).run()
+  RunnableGraph.fromGraph(graph).run()
 }
